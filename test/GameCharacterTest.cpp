327608ab870a7e0bb945cc7da8545b6b9e3c704e
alberto-pizzi <alberto.pizzi2002@outlook.com>
2022-11-29 17:38:14 +0100

    Fixato UnitTesting

--- test/GameCharacterTest.cpp
+++ test/GameCharacterTest.cpp
@@ -2,6 +2,7 @@
 
 #include "../GameCharacter.h"
 #include "../Mike.h"
+#include "../Gui.h"
 
 //FIXME fix all unit testing (fixture)
 
@@ -13,15 +14,14 @@ TEST (Mike, CorrectSpriteCenter) {
     EXPECT_EQ(16, mike.getPos().x);
     EXPECT_EQ(16, mike.getPos().y);
 }
-/*
+
 TEST (HealthBar, TotalHPTest) {
     sf::Context context;
     sf::Texture tex;
     Mike mike(tex, 0, 0);
-    //Gui gui;
+    Gui gui;
 
     //HP must be equal to Up
     EXPECT_EQ(20, mike.getHp());
-    //EXPECT_EQ(5*32, gui.getTexManager().getTextureRef("healthBar").getSize().x);
+    EXPECT_EQ(5 * 32, gui.getTexManager().getTextureRef("healthBar").getSize().x);
 }
-*/
